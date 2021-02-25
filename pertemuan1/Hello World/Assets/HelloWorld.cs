a0e8321acffd9962bb2181c65eeae4b59bc801d6
anggistaamaliayashinta <anggistaamalia@gmail.com>
2021-02-26 05:26:53 +0700

    pertemuan1

---
+++ pertemuan1/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour
+{
+    public string myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI(){
+         GUILayout.BeginArea(new Rect(10,10,200,200));
+         GUILayout.Label(myText);
+         GUILayout.EndArea();
+}
+}
